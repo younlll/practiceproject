0e6846c14a72e090da1c5677df63e0ad71e022f6
younlll <youn.lee96@gmail.com>
2022-09-09 13:33:51 +0900

    init

---
+++ src/main/java/practice/practiceproject/PracticeprojectApplication.java
@@ -0,0 +1,13 @@
+package practice.practiceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/practiceproject/PracticeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.practiceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
